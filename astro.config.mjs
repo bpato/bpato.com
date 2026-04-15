5a72d87b5e70ed352c4c8ada8a907e6d1c38a92b
bpato <bpato@users.noreply.github.com>
2026-04-16 01:02:53 +0200

    Update import for env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
-import { loadEnv } from "vite";
-
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site: env.DOMAIN,
+  site: import.meta.env.DOMAIN,
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
